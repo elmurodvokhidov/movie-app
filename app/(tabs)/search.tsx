3ff172be9cf817042807ceb78229a3fb08e599dc
unknown <elmurodvkh@gmail.com>
2025-04-04 10:46:57 +0500

    appwrite setup

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -6,6 +6,7 @@ import { fetchMovies } from '@/services/api';
 import { icons } from '@/constants/icons';
 import SearchBar from '@/components/SearchBar';
 import { useEffect, useState } from 'react';
+import { updateSearchCount } from '@/services/appwrite';
 
 export default function Search() {
     const [searchQuery, setSearchQuery] = useState("");
@@ -24,6 +25,9 @@ export default function Search() {
         const timeoutId = setTimeout(async () => {
             if (searchQuery.trim()) {
                 await loadMovies();
+                if (movies?.length > 0 && movies?.[0]) {
+                    await updateSearchCount(searchQuery, movies[0]);
+                }
             } else {
                 reset();
             }
